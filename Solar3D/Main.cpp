c395afa38c827fb0ebf5c212da7e8959d7561d1d
Jianming <JLiang@esri.com>
2020-06-05 22:00:15 -0700

    Temp.

--- Solar3D/Main.cpp
+++ Solar3D/Main.cpp
@@ -45,7 +45,6 @@
 #include <osgEarth/Metrics>
 #include <iostream>
 
-#define LC "[viewer] "
 
 using namespace osgEarth::Symbology;
 using namespace osgEarth;
@@ -56,6 +55,7 @@ using namespace osgEarth::Util;
 
 SolarParam m_solarParam;
 osg::ref_ptr<SkyViewFactorEventHandler> m_skyViewHandler;
+const int UI_FONT_SIZE = 18;
 
 SolarParam createSolarParam()
 {
@@ -70,7 +70,7 @@ SolarParam createSolarParam()
   param.startDay = param.day;
   param.endDay = param.day;
   param.isSingleDay = true;
-  param.elev = 500;
+  param.elev = 0;
   return param;
 }
 
@@ -153,17 +153,14 @@ public:
   void Init(std::string name, std::string label, bool isInteger)
   {
     m_isInteger = isInteger;
-
-    setChildSpacing(10);
+    setChildSpacing(0);
     setChildVertAlign(CustomControls::Control::ALIGN_CENTER);
     setHorizFill(true);
     m_nameLabel = new CustomControls::LabelControl(label);
     m_nameLabel->setHorizAlign(CustomControls::Control::ALIGN_LEFT);
     m_nameLabel->setVertAlign(CustomControls::Control::ALIGN_CENTER);
     m_nameLabel->setTextBackdropOffset(3);
-    //m_nameLabel->setBackColor(.6, .6, 0, 1);
-    //m_nameLabel->setForeColor(0, 0, 0, 1);
-    m_nameLabel->setHorizFill(true, 200);
+    m_nameLabel->setFontSize(UI_FONT_SIZE);
     addControl(m_nameLabel.get());
 
   }
@@ -174,7 +171,7 @@ public:
 
     m_slider = new CustomControls::HSliderControl(min, max, value);
     //m_slider->setBackColor(.6, 0, 0, 1);
-    m_slider->setHeight(25);
+    m_slider->setHeight(UI_FONT_SIZE);
     m_slider->setWidth(200);
     m_slider->setName(name);
     m_slider->addEventHandler(this);
@@ -192,7 +189,7 @@ public:
 
     m_check = new CustomControls::CheckBoxControl(isChecked);
     //slider->setBackColor(.6, 0, 0, 1);
-    m_check->setHeight(25);
+    m_check->setHeight(10);
     m_check->setName(name);
     m_check->addEventHandler(this);
     addControl(m_check.get());
@@ -206,15 +203,16 @@ public:
     m_slider = new CustomControls::HSliderControl(min, max, value);
     m_slider->setHorizAlign(CustomControls::Control::ALIGN_LEFT);
     //m_slider->setBackColor(.6, 0, 0, 1);
-    m_slider->setHeight(25);
+    m_slider->setHeight(10);
     m_slider->setWidth(200);
     m_slider->setName(name);
     m_slider->addEventHandler(this);
     m_valueLabel = new CustomControls::LabelControl(name);
     m_valueLabel->setText(toString(value));
+    m_valueLabel->setFontSize(UI_FONT_SIZE);
     m_check = new CustomControls::CheckBoxControl(isChecked);
     //slider->setBackColor(.6, 0, 0, 1);
-    m_check->setHeight(25);
+    m_check->setHeight(10);
     m_check->setName(name);
     m_check->addEventHandler(this);
     addControl(m_slider.get());
@@ -337,7 +335,7 @@ void createControls(CustomControls::ControlCanvas* cs)
 {
   osg::ref_ptr<CustomControls::VBox> ul = new CustomControls::VBox();
   ul->setPosition(0, 0);
-  ul->setPadding(10);
+  ul->setPadding(5);
   int maxLabelLen = 20;
   osg::Vec4 backgroundColor(0.0, 0.0, 1.0, 0.3);
   osg::Vec4 borderColor(0.0, 0.0, 0.0, 1.0);
@@ -355,14 +353,15 @@ void createControls(CustomControls::ControlCanvas* cs)
   m_parametersControl = new CustomControls::VBox();
   m_parametersControl->setBackColor(backgroundColor);
   m_parametersControl->setBorderColor(borderColor);
-  osg::ref_ptr <CustomControls::LabelControl> titleLabel = new CustomControls::LabelControl("GRASS GIS r.sun parameters\n(https://grass.osgeo.org/grass78/manuals/r.sun.html)");
+  osg::ref_ptr <CustomControls::LabelControl> titleLabel = new CustomControls::LabelControl("GRASS GIS r.sun parameters");
+  titleLabel->setFontSize(UI_FONT_SIZE);
   osg::ref_ptr<CustomControls::HBox> linkieSlider = new ParamControl("Linkie", PadRight("Linkie", maxLabelLen), 3, 8, m_solarParam.linke, true);
   osg::ref_ptr<CustomControls::HBox> startDaySlider = new ParamControl("StartDay", PadRight("Start Day", maxLabelLen), 1, 365, m_solarParam.startDay, true);
   osg::ref_ptr<CustomControls::HBox> endDaySlider = new ParamControl("EndDay", PadRight("End Day", maxLabelLen), 1, 365, m_solarParam.endDay, true);
   osg::ref_ptr<CustomControls::HBox> isSingleDayCheck = new ParamControl("SingleDayMode", PadRight("Single Day Mode", maxLabelLen), m_solarParam.isSingleDay);
   osg::ref_ptr<CustomControls::HBox> timeStepSlider = new ParamControl("TimeStep", PadRight("Time Step (hours)", maxLabelLen), 0.1, 1, m_solarParam.time_step, false);
   osg::ref_ptr<CustomControls::HBox> latSlider = new ParamControl("Latitude", PadRight("Default Latitude", maxLabelLen), -90, 90, m_solarParam.lat, false, true);
-  osg::ref_ptr<CustomControls::HBox> elevSlider = new ParamControl("Elevation", PadRight("Default Base Elevation", maxLabelLen), 0, 9999, m_solarParam.elev, false, true);
+  osg::ref_ptr<CustomControls::HBox> elevSlider = new ParamControl("Elevation", PadRight("Base Elevation", maxLabelLen), 0, 9999, m_solarParam.elev, false, true);
   m_parametersControl->addControl(titleLabel.get());
   m_parametersControl->addControl(isSingleDayCheck.get());
   m_parametersControl->addControl(startDaySlider.get());
@@ -377,10 +376,10 @@ void createControls(CustomControls::ControlCanvas* cs)
   m_resultLabelsControl->setBorderColor(borderColor);
 
   osg::Vec4 resultFontColor(1, 1, 0, 1);
-  m_svfLabel = new CustomControls::LabelControl("SVF:", resultFontColor, 26);
-  m_globalRadLabel = new CustomControls::LabelControl("Global radiation:", resultFontColor, 26);
-  m_beamRadLabel = new CustomControls::LabelControl("Beam radiation:", resultFontColor, 26);
-  m_diffuseRadLabel = new CustomControls::LabelControl("Diffuse radiation:", resultFontColor, 26);
+  m_svfLabel = new CustomControls::LabelControl("SVF:", resultFontColor, UI_FONT_SIZE);
+  m_globalRadLabel = new CustomControls::LabelControl("Global radiation:", resultFontColor, UI_FONT_SIZE);
+  m_beamRadLabel = new CustomControls::LabelControl("Beam radiation:", resultFontColor, UI_FONT_SIZE);
+  m_diffuseRadLabel = new CustomControls::LabelControl("Diffuse radiation:", resultFontColor, UI_FONT_SIZE);
 
   m_resultLabelsControl->addControl(m_svfLabel.get());
   m_resultLabelsControl->addControl(m_globalRadLabel.get());
@@ -391,8 +390,8 @@ void createControls(CustomControls::ControlCanvas* cs)
   //m_fisheyeControl->setBackColor(backgroundColor);
   m_fisheyeControl->setBorderColor(borderColor);
   osg::ref_ptr<CustomControls::ImageControl> fishEyeImg = new CustomControls::ImageControl(m_skyViewHandler->_cubemap2fisheyeCamera->Texture());
+  fishEyeImg->setSize(320, 320);
   m_fisheyeControl->addControl(fishEyeImg.get());
-
   ul->addControl(togglesControl.get());
   ul->addControl(m_parametersControl.get());
   ul->addControl(m_resultLabelsControl.get());
@@ -400,249 +399,6 @@ void createControls(CustomControls::ControlCanvas* cs)
 
   cs->addControl(ul.get());
 }
-
-#define IMAGERY_URL      "http://readymap.org/readymap/tiles/1.0.0/22/"
-#define ELEVATION_URL    "http://readymap.org/readymap/tiles/1.0.0/116/"
-#define BUILDINGS_URL    "./data/boston_buildings_utm19.shp"
-#define RESOURCE_LIB_URL "./data/resources/textures_us/catalog.xml"
-#define STREETS_URL      "./data/boston-scl-utm19n-meters.shp"
-#define PARKS_URL        "./data/boston-parks.shp"
-#define TREE_MODEL_URL   "./data/tree.osg"
-
-// forward declarations.
-void addImagery(Map* map);
-void addElevation(Map* map);
-void addBuildings(Map* map);
-void addStreets(Map* map);
-void addParks(Map* map);
-
-osgEarth::ProfileOptions ProfileOptionsFromFile(std::string filename)
-{
-  osgEarth::ProfileOptions opt;
-  if (filename.substr(filename.length() - 4, 4) == ".shp")
-  {
-    char srs[512];
-    memset(srs, 0, 512);
-    char* psrs = (char*)srs;
-    ShapeFile shp(filename);
-    shp.poLayer->GetSpatialRef()->exportToWkt(&psrs);
-    opt.srsString() = psrs;
-    OGREnvelope env;
-    shp.poLayer->GetExtent(&env);
-    opt.bounds() = osgEarth::Bounds(env.MinX, env.MinY, env.MaxX, env.MaxY);
-  }
-  else
-  {
-    double adfGeoTransform[6];
-    GDALDataset* pDataset = (GDALDataset*)GDALOpen(filename.data(), GA_ReadOnly);
-    pDataset->GetGeoTransform(adfGeoTransform);
-    int ncols = pDataset->GetRasterXSize();
-    int nrows = pDataset->GetRasterYSize();
-    opt.bounds() = osgEarth::Bounds(adfGeoTransform[0], adfGeoTransform[3] + adfGeoTransform[5] * nrows, adfGeoTransform[3], adfGeoTransform[0] + adfGeoTransform[1] * ncols);
-    opt.srsString() = pDataset->GetProjectionRef();
-  }
-  return opt;
-}
-
-
-void addImagery(Map* map)
-{
-  // add a TMS imagery layer:
-  TMSOptions imagery;
-  imagery.url() = IMAGERY_URL;
-  map->addLayer(new ImageLayer("ReadyMap imagery", imagery));
-}
-
-
-void addElevation(Map* map)
-{
-  // add a TMS elevation layer:
-  TMSOptions elevation;
-  elevation.url() = ELEVATION_URL;
-  map->addLayer(new ElevationLayer("ReadyMap elevation", elevation));
-}
-
-
-void addBuildings(Map* map)
-{
-  // create a feature source to load the building footprint shapefile.
-  OGRFeatureOptions buildingData;
-  buildingData.name() = "buildings";
-  buildingData.url() = BUILDINGS_URL;
-  buildingData.buildSpatialIndex() = true;
-
-  // a style for the building data:
-  Style buildingStyle;
-  buildingStyle.setName("buildings");
-
-  // Extrude the shapes into 3D buildings.
-  ExtrusionSymbol* extrusion = buildingStyle.getOrCreate<ExtrusionSymbol>();
-  extrusion->heightExpression() = NumericExpression("3.5 * max( [story_ht_], 1 )");
-  extrusion->flatten() = true;
-  extrusion->wallStyleName() = "building-wall";
-  extrusion->roofStyleName() = "building-roof";
-
-  PolygonSymbol* poly = buildingStyle.getOrCreate<PolygonSymbol>();
-  poly->fill()->color() = Color::White;
-
-  // Clamp the buildings to the terrain.
-  AltitudeSymbol* alt = buildingStyle.getOrCreate<AltitudeSymbol>();
-  alt->clamping() = alt->CLAMP_TO_TERRAIN;
-  alt->binding() = alt->BINDING_VERTEX;
-
-  // a style for the wall textures:
-  Style wallStyle;
-  wallStyle.setName("building-wall");
-  SkinSymbol* wallSkin = wallStyle.getOrCreate<SkinSymbol>();
-  wallSkin->library() = "us_resources";
-  wallSkin->addTag("building");
-  wallSkin->randomSeed() = 1;
-
-  // a style for the rooftop textures:
-  Style roofStyle;
-  roofStyle.setName("building-roof");
-  SkinSymbol* roofSkin = roofStyle.getOrCreate<SkinSymbol>();
-  roofSkin->library() = "us_resources";
-  roofSkin->addTag("rooftop");
-  roofSkin->randomSeed() = 1;
-  roofSkin->isTiled() = true;
-
-  // assemble a stylesheet and add our styles to it:
-  StyleSheet* styleSheet = new StyleSheet();
-  styleSheet->addStyle(buildingStyle);
-  styleSheet->addStyle(wallStyle);
-  styleSheet->addStyle(roofStyle);
-
-  // load a resource library that contains the building textures.
-  ResourceLibrary* reslib = new ResourceLibrary("us_resources", RESOURCE_LIB_URL);
-  styleSheet->addResourceLibrary(reslib);
-
-  // set up a paging layout for incremental loading. The tile size factor and
-  // the visibility range combine to determine the tile size, such that
-  // tile radius = max range / tile size factor.
-  FeatureDisplayLayout layout;
-  layout.tileSize() = 500;
-  layout.addLevel(FeatureLevel(0.0f, 20000.0f, "buildings"));
-
-  FeatureModelLayer* layer = new FeatureModelLayer();
-  layer->setName("Buildings");
-  layer->options().featureSource() = buildingData;
-  layer->options().styles() = styleSheet;
-  layer->options().layout() = layout;
-
-  map->addLayer(layer);
-}
-
-
-void addStreets(Map* map)
-{
-  // create a feature source to load the street shapefile.
-  OGRFeatureOptions feature_opt;
-  feature_opt.name() = "streets";
-  feature_opt.url() = STREETS_URL;
-  feature_opt.buildSpatialIndex() = true;
-
-  // a resampling filter will ensure that the length of each segment falls
-  // within the specified range. That can be helpful to avoid cropping 
-  // very long lines segments.
-  ResampleFilterOptions resample;
-  resample.minLength() = 0.0f;
-  resample.maxLength() = 25.0f;
-  feature_opt.filters().push_back(resample);
-
-  // a style:
-  Style style;
-  style.setName("streets");
-
-  // Render the data as translucent yellow lines that are 7.5m wide.
-  LineSymbol* line = style.getOrCreate<LineSymbol>();
-  line->stroke()->color() = Color(Color::Yellow, 0.5f);
-  line->stroke()->width() = 7.5f;
-  line->stroke()->widthUnits() = Units::METERS;
-
-  // Clamp the lines to the terrain.
-  AltitudeSymbol* alt = style.getOrCreate<AltitudeSymbol>();
-  alt->clamping() = alt->CLAMP_TO_TERRAIN;
-
-  // Apply a depth offset to avoid z-fighting. The "min bias" is the minimum
-  // apparent offset (towards the camera) of the geometry from its actual position.
-  // The value here was chosen empirically by tweaking the "oe_doff_min_bias" uniform.
-  RenderSymbol* render = style.getOrCreate<RenderSymbol>();
-  render->depthOffset()->minBias() = 6.6f;
-
-  // Set up a paging layout. The tile size factor and the visibility range combine
-  // to determine the tile size, such that tile radius = max range / tile size factor.
-  FeatureDisplayLayout layout;
-  layout.tileSize() = 500;
-  layout.maxRange() = 5000.0f;
-
-  // create a model layer that will render the buildings according to our style sheet.
-  FeatureModelLayerOptions streets;
-  streets.name() = "streets";
-  streets.featureSource() = feature_opt;
-  streets.layout() = layout;
-  streets.styles() = new StyleSheet();
-  streets.styles()->addStyle(style);
-
-  map->addLayer(new FeatureModelLayer(streets));
-}
-
-
-void addParks(Map* map)
-{
-  // create a feature source to load the shapefile.
-  OGRFeatureOptions parksData;
-  parksData.name() = "parks";
-  parksData.url() = PARKS_URL;
-  parksData.buildSpatialIndex() = true;
-
-  // a style:
-  Style style;
-  style.setName("parks");
-
-  // Render the data using point-model substitution, which replaces each point
-  // in the feature geometry with an instance of a 3D model. Since the input
-  // data are polygons, the PLACEMENT_RANDOM directive below will scatter
-  // points within the polygon boundary at the specified density.
-  ModelSymbol* model = style.getOrCreate<ModelSymbol>();
-  model->url()->setLiteral(TREE_MODEL_URL);
-  model->placement() = model->PLACEMENT_RANDOM;
-  model->density() = 6000.0f; // instances per sqkm
-
-  // Clamp to the terrain:
-  AltitudeSymbol* alt = style.getOrCreate<AltitudeSymbol>();
-  alt->clamping() = alt->CLAMP_TO_TERRAIN;
-
-  // Since the tree model contains alpha components, we will discard any data
-  // that's sufficiently transparent; this will prevent depth-sorting anomalies
-  // common when rendering lots of semi-transparent objects.
-  RenderSymbol* render = style.getOrCreate<RenderSymbol>();
-  render->transparent() = true;
-  render->minAlpha() = 0.15f;
-
-  // Set up a paging layout. The tile size factor and the visibility range combine
-  // to determine the tile size, such that tile radius = max range / tile size factor.
-  FeatureDisplayLayout layout;
-  layout.tileSize() = 650;
-  layout.addLevel(FeatureLevel(0.0f, 2000.0f, "parks"));
-
-  // create a model layer that will render the buildings according to our style sheet.
-  FeatureModelLayerOptions parks;
-  parks.name() = "parks";
-  parks.featureSource() = parksData;
-  parks.layout() = layout;
-  parks.styles() = new StyleSheet();
-  parks.styles()->addStyle(style);
-
-  Layer* parksLayer = new FeatureModelLayer(parks);
-  map->addLayer(parksLayer);
-
-  if (parksLayer->getStatus().isError())
-  {
-    OE_WARN << parksLayer->getStatus().message() << std::endl;
-  }
-}
-
 
 int main(int argc, char** argv)
 {
@@ -665,33 +421,48 @@ int main(int argc, char** argv)
   // prevents the "unsupported wrapper" messages from OSG
   osgDB::Registry::instance()->getObjectWrapperManager()->findWrapper("osg::Image");
 
-  // install our default manipulator (do this before calling load)
-  osg::ref_ptr<EarthManipulator> manip = new EarthManipulator(arguments);
-  viewer.setCameraManipulator(manip.get());
-
-  // disable the small-feature culling
-  viewer.getCamera()->setSmallFeatureCullingPixelSize(-1.0f);
-
-  // set a near/far ratio that is smaller than the default. This allows us to get
-  // closer to the ground without near clipping. If you need more, use --logdepth
-  viewer.getCamera()->setNearFarRatio(0.0001);
-
   // load an earth file, and support all or our example command-line options
   // and earth file <external> tags    
-  osg::ref_ptr<osg::Node> node = MapNodeHelper().load(arguments, &viewer);
-  if (!node)
-    return 0;
+  osg::ref_ptr<osg::Node> scene = MapNodeHelper().load(arguments, &viewer);
+  if (!scene)
+  {
+    scene = osgDB::readNodeFiles(arguments);
+  }
+
   osg::ref_ptr<osg::Group> root = new osg::Group;
-  root->addChild(node.get());
+  root->addChild(scene.get());
+
   viewer.setSceneData(root.get());
-  MapNode* mapNode = MapNode::findMapNode(node);
-  m_skyViewHandler = new SkyViewFactorEventHandler(mapNode, root.get(), manip.get(), &viewer, &m_solarParam, ResultsUpdated);
+
+  MapNode* mapNode = MapNode::findMapNode(scene);
+  // install our default manipulator (do this before calling load)
+  osg::ref_ptr<osgGA::CameraManipulator> manip;
+  if (mapNode)
+  {
+    manip = new EarthManipulator(arguments);
+
+    // disable the small-feature culling
+    viewer.getCamera()->setSmallFeatureCullingPixelSize(-1.0f);
+
+    // set a near/far ratio that is smaller than the default. This allows us to get
+    // closer to the ground without near clipping. If you need more, use --logdepth
+    viewer.getCamera()->setNearFarRatio(0.0001);
+  }
+  else
+  {
+    manip = new osgGA::TrackballManipulator();
+  }
+
+  viewer.setCameraManipulator(manip.get());
+
+  m_skyViewHandler = new SkyViewFactorEventHandler(mapNode ? mapNode : scene.get(), root.get(), manip.get(), &viewer, &m_solarParam, ResultsUpdated);
  
   // create a surface to house the controls
   CustomControls::ControlCanvas* cs = CustomControls::ControlCanvas::getOrCreate(&viewer);
 
   // create some controls.
   createControls(cs);
+
   // zoom to a good startup position
 
   viewer.addEventHandler(m_skyViewHandler.get());
@@ -706,7 +477,20 @@ int main(int argc, char** argv)
 
   // add the LOD Scale handler
   viewer.addEventHandler(new osgViewer::LODScaleHandler);
-  viewer.getCamera()->setViewport(50, 50, 1600, 1024);
-  Metrics::run(viewer);
-  return 0;
+
+  unsigned int width, height;
+  osg::GraphicsContext::ScreenIdentifier main_screen_id;
+  osg::GraphicsContext::WindowingSystemInterface* wsi = osg::GraphicsContext::getWindowingSystemInterface();
+  main_screen_id.readDISPLAY();
+  main_screen_id.setUndefinedScreenDetailsToDefaultScreen();
+  wsi->getScreenResolution(main_screen_id, width, height);
+
+  printf("%d,%d\n", width, height);
+  //viewer.getCamera()->setViewport(50, 50, 1600, 1024);
+  if (mapNode)
+  {
+    Metrics::run(viewer);
+    return 0;
+  }
+  return viewer.run();
 }
